8c349368f6323127a6c5512f07c3269e7c7859b7
Deniansyah <deniansyah814@gmail.com>
2023-01-07 19:13:35 +0700

    Sudah siap untuk mulai slicing

---
+++ src/screens/Login.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+  // SafeAreaView,
+  // ScrollView,
+  // StatusBar,
+  // StyleSheet,
+  Text,
+  // useColorScheme,
+  View,
+} from 'react-native';
+
+const Login = () => {
+  return (
+    <View>
+      <Text>Login Page</Text>
+    </View>
+  );
+};
+
+export default Login;
